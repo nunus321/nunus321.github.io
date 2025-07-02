46e0d9e2c466b70e8ab760c142b504be2e8774d9
nunus321 <jonasalmersal@outlook.dk>
2025-07-02 23:27:26 +0200

    m

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./", // Add this line for GitHub Pages compatibility
+  plugins: [checker({ typescript: true })],
+  worker: {},
+  build: {
+    sourcemap: false,
+  },
+  server: {
+    open: true,
+    port: 1234,
+    host: "localhost",
+  },
+});
